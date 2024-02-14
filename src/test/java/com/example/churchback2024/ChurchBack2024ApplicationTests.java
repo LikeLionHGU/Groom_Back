d41578d8d2e00f1954ab015400ec3af6f5700f21
uojin29 <uojin29@gmail.com>
2024-02-14 20:05:38 +0900

    feat: init

---
+++ src/test/java/com/example/churchback2024/ChurchBack2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.churchback2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChurchBack2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
