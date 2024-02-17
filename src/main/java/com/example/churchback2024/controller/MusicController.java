1da253abd85d75bfac4bc3b9ef5dedf4a31775d0
dlwjdaud <reaudwjd@handong.ac.kr>
2024-02-17 18:34:02 +0900

    feat: MusicController 내용 구현

--- src/main/java/com/example/churchback2024/controller/MusicController.java
+++ src/main/java/com/example/churchback2024/controller/MusicController.java
@@ -1,14 +1,44 @@
 package com.example.churchback2024.controller;
 
-
+import com.example.churchback2024.controller.request.music.MusicCreateRequest;
+import com.example.churchback2024.controller.request.music.MusicUpdateRequest;
+import com.example.churchback2024.controller.response.music.MusicListResponse;
+import com.example.churchback2024.controller.response.music.MusicResponse;
+import com.example.churchback2024.domain.Music;
+import com.example.churchback2024.dto.MusicDto;
+import com.example.churchback2024.service.MusicService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin("*")
-@RequestMapping("/church+/sheetmusic")
+@RequestMapping("/church+/music")
 public class MusicController {
+    private final MusicService musicService;
+
+    @GetMapping("/list")
+    public ResponseEntity<MusicListResponse> findMusicList() {
+        MusicListResponse musicListResponse = musicService.getMusicList();
+        return ResponseEntity.ok(musicListResponse);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<MusicResponse> createMusic(@RequestBody MusicCreateRequest request) {
+        MusicDto musicDto = musicService.createMusic(MusicDto.from(request));
+        return ResponseEntity.ok(new MusicResponse(musicDto));
+    }
+
+    @PatchMapping("/{musicId}")
+    public ResponseEntity<Music> updateMusic(@PathVariable Long musicId, @RequestBody MusicUpdateRequest request) {
+        return ResponseEntity.ok(musicService.updateMusic(musicId, MusicDto.from(request)));
+    }
+
+    @DeleteMapping("/{musicId}")
+    public ResponseEntity<Void> deleteMusic(@PathVariable Long musicId) {
+        musicService.deleteMusic(musicId);
+        return ResponseEntity.noContent().build();
+    }
 }
+
