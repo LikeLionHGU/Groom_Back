f3e33a035e40cba3127c1243df9155bc12f4fc6d
uojin29 <uojin29@gmail.com>
2024-02-20 09:46:06 +0900

    feat: 필터링 request

---
+++ src/main/java/com/example/churchback2024/controller/request/music/CodeSearchRequest.java
@@ -0,0 +1,12 @@
+package com.example.churchback2024.controller.request.music;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Data
+public class CodeSearchRequest {
+    private String code;
+}
