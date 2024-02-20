bb43baaea8e8a1a6aeadfcdc90ee4a3fb5456321
uojin29 <uojin29@gmail.com>
2024-02-20 09:48:55 +0900

    feat: 폴더 연관관계, 폴더 조회 추가
    - 폴더 & 멤버그룹 연관관계 설정
    - 학생의 메일, 그룹명, path를 이용해서 폴더 조회 (로그인 한 학생이 가입한 그룹의 path안의 폴더)

--- src/main/java/com/example/churchback2024/dto/FolderDto.java
+++ src/main/java/com/example/churchback2024/dto/FolderDto.java
@@ -2,6 +2,7 @@ package com.example.churchback2024.dto;
 
 import com.example.churchback2024.controller.request.Folder.FolderCreateRequest;
 import com.example.churchback2024.controller.request.Folder.FolderUpdateRequest;
+import com.example.churchback2024.domain.MemberGroup;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,11 +17,15 @@ public class FolderDto {
     private Long folderId;
     private String folderName;
     private String path;
+    private Long memberId;
+    private Long groupId;
 
     public static FolderDto from(FolderCreateRequest request) {
         return FolderDto.builder()
                 .folderName(request.getFolderName())
                 .path(request.getPath())
+                .memberId(request.getMemberId())
+                .groupId(request.getGroupId())
                 .build();
     }
 
--- src/main/java/com/example/churchback2024/service/FolderService.java
+++ src/main/java/com/example/churchback2024/service/FolderService.java
@@ -3,10 +3,19 @@ package com.example.churchback2024.service;
 import com.example.churchback2024.controller.response.folder.FolderListResponse;
 import com.example.churchback2024.controller.response.folder.FolderResponse;
 import com.example.churchback2024.domain.Folder;
+import com.example.churchback2024.domain.GroupC;
+import com.example.churchback2024.domain.Member;
+import com.example.churchback2024.domain.MemberGroup;
 import com.example.churchback2024.dto.FolderDto;
 import com.example.churchback2024.exception.folder.DuplicateFolderException;
 import com.example.churchback2024.exception.folder.FolderNotFoundException;
+import com.example.churchback2024.exception.group.GroupNotFoundException;
+import com.example.churchback2024.exception.groupMember.MemberGroupNotFoundException;
+import com.example.churchback2024.exception.member.MemberNotFoundException;
 import com.example.churchback2024.repository.FolderRepository;
+import com.example.churchback2024.repository.GroupRepository;
+import com.example.churchback2024.repository.MemberGroupRepository;
+import com.example.churchback2024.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,13 +28,21 @@ import java.util.stream.Collectors;
 @Transactional
 public class FolderService {
     private final FolderRepository folderRepository;
+    private final MemberGroupRepository memberGroupRepository;
+    private final MemberRepository memberRepository;
+    private final GroupRepository groupRepository;
 
     public void createFolder(FolderDto folderDto) {
-        Folder folder = folderRepository.findByFolderName(folderDto.getFolderName());
+        MemberGroup memberGroup = memberGroupRepository.findByMember_MemberIdAndGroupC_GroupId(folderDto.getMemberId(), folderDto.getGroupId());
+        Folder folder = folderRepository.findByFolderNameAndMemberGroup(folderDto.getFolderName(), memberGroup);
+
         if(folder != null){
             throw new DuplicateFolderException();
         }
-        folderRepository.save(Folder.from(folderDto));
+        if(memberGroup == null){
+            throw new MemberGroupNotFoundException();
+        }
+        folderRepository.save(Folder.from(folderDto, memberGroup));
     }
 
     public FolderListResponse getFolderList() {
@@ -58,6 +75,25 @@ public class FolderService {
     }
 
 
+    public FolderResponse getFolderByMemberAndGroupAndPath(String email, String groupName, String path) {
+        Member member = memberRepository.findByEmail(email);
+        if (member == null) {
+            throw new MemberNotFoundException();
+        }
+        GroupC group = groupRepository.findByGroupName(groupName);
+        if (group == null) {
+            throw new GroupNotFoundException();
+        }
+        MemberGroup memberGroup = memberGroupRepository.findByMember_MemberIdAndGroupC_GroupId(member.getMemberId(), group.getGroupId());
+        if (memberGroup == null) {
+            throw new MemberGroupNotFoundException();
+        }
+        Folder folder = folderRepository.findByPathAndMemberGroup(path, memberGroup);
+        if (folder == null) {
+            throw new FolderNotFoundException();
+        }
+        return new FolderResponse(folder);
+    }
 }
 
 // 완성
