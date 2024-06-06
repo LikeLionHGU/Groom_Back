0c383fc7265d149784a1c71db6a519d333faca7f
uojin29 <uojin29@gmail.com>
2024-06-06 23:43:30 +0900

    feat: response에 isNew 추가

--- src/main/java/com/example/churchback2024/controller/response/member/MemberResponse.java
+++ src/main/java/com/example/churchback2024/controller/response/member/MemberResponse.java
@@ -12,9 +12,11 @@ public class MemberResponse {
     private String name;
     private String email;
     private Long memberId;
+    private Boolean isNew;
     public MemberResponse(MemberDto memberDto) {
         this.name = memberDto.getName();
         this.email = memberDto.getEmail();
         this.memberId = memberDto.getMemberId();
+        this.isNew = memberDto.getIsNew();
     }
 }
