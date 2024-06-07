e551b83391c92adc082f837644d8cbb5cbed30c7
uojin29 <uojin29@gmail.com>
2024-06-07 21:21:29 +0900

    feat: 그룹 중복 exception에서 회원 -> 그룹

--- src/main/java/com/example/churchback2024/exception/group/DuplicateGroupException.java
+++ src/main/java/com/example/churchback2024/exception/group/DuplicateGroupException.java
@@ -2,6 +2,6 @@ package com.example.churchback2024.exception.group;
 
 public class DuplicateGroupException extends RuntimeException {
     public DuplicateGroupException() {
-        super("이미 존재하는 회원입니다.");
+        super("이미 존재하는 그룹입니다.");
     }
 }
